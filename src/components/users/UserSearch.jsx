153aa6286d5c55ec833a59ff57917ce26d3f4b49
mariusz17 <qbajt.mariusz.kubat@gmail.com>
2022-04-26 20:27:59 +0200

    add search user from the form

--- src/components/users/UserSearch.jsx
+++ src/components/users/UserSearch.jsx
@@ -3,7 +3,7 @@ import GithubContext from "../../context/github/GithubContext";
 
 function UserSearch() {
 	const [text, setText] = useState("");
-	const { users } = useContext(GithubContext);
+	const { users, searchUsers } = useContext(GithubContext);
 
 	const handleChange = (e) => {
 		setText(e.target.value);
@@ -14,7 +14,9 @@ function UserSearch() {
 		if (text === "") {
 			alert("Please enter something.");
 		} else {
-			// @todo search users
+			searchUsers(text);
+
+			setText("");
 		}
 	};
 
--- src/context/github/GithubContext.js
+++ src/context/github/GithubContext.js
@@ -19,20 +19,23 @@ export const GithubProvider = ({ children }) => {
 
 	const setLoading = () => dispatch({ type: "SET_LOADING" });
 
-	// Get some users for testing
-	const fetchUsers = async () => {
+	const searchUsers = async (text) => {
 		setLoading();
 
-		const response = await fetch(`${GITHUB_URL}/users`, {
+		const params = new URLSearchParams({
+			q: text,
+		});
+
+		const response = await fetch(`${GITHUB_URL}/search/users?${params}`, {
 			headers: {
 				Authorization: `token ${GITHUB_TOKEN}`,
 			},
 		});
-		const data = await response.json();
+		const { items } = await response.json();
 
 		dispatch({
 			type: "GET_USERS",
-			payload: data,
+			payload: items,
 		});
 
 		// setUsers(data);
@@ -44,7 +47,7 @@ export const GithubProvider = ({ children }) => {
 			value={{
 				users: state.users,
 				loading: state.loading,
-				fetchUsers,
+				searchUsers,
 			}}
 		>
 			{children}
