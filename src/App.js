cd247c9243f48c52ce38951da44952ec1c2504b9
mariusz17 <qbajt.mariusz.kubat@gmail.com>
2022-04-27 15:33:20 +0200

    add Alert component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./components/layout/Navbar";
 import Home from "./pages/Home";
 import About from "./pages/About";
 import NotFound from "./pages/NotFound";
-import Navbar from "./components/layout/Navbar";
+import Alert from "./components/layout/Alert";
 import Footer from "./components/layout/Footer";
 import { GithubProvider } from "./context/github/GithubContext";
 import { AlertProvider } from "./context/alert/AlertContext";
@@ -16,6 +17,7 @@ function App() {
 						<Navbar />
 
 						<main className="container mx-auto px-3 pb-12">
+							<Alert />
 							<Routes>
 								<Route path="/" element={<Home />} />
 								<Route path="/about" element={<About />} />
