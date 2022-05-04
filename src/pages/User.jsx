019a19c82442999e33582681cd89a49a40ee8b40
mariusz17 <qbajt.mariusz.kubat@gmail.com>
2022-05-04 16:48:09 +0200

    clean up actions and add axios

--- src/pages/User.jsx
+++ src/pages/User.jsx
@@ -4,7 +4,7 @@ import { Link, useParams } from "react-router-dom";
 import GithubContext from "../context/github/GithubContext";
 import Spinner from "../components/layout/Spinner";
 import RepoList from "../components/repos/RepoList";
-import { getUser, getUserRepos } from "../context/github/GithubActions";
+import { getUserAndRepos } from "../context/github/GithubActions";
 
 function User() {
 	const { loading, dispatch, repos, user } = useContext(GithubContext);
@@ -15,16 +15,10 @@ function User() {
 		dispatch({ type: "SET_LOADING" });
 
 		const getUserData = async () => {
-			const userData = await getUser(params.login);
+			const userData = await getUserAndRepos(params.login);
 			dispatch({
-				type: "GET_USER",
+				type: "GET_USER_AND_REPOS",
 				payload: userData,
-			});
-
-			const userRepos = await getUserRepos(params.login);
-			dispatch({
-				type: "GET_REPOS",
-				payload: userRepos,
 			});
 		};
 
